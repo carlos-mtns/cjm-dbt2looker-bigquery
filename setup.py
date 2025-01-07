8cb2d187e8926536cf85299642d5178e4d476456
carlos-mtns <carlos.martins@devoteam.com>
2025-01-07 09:54:12 +0100

    c1

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cjm-dbt2looker-bigquery',
+    version='0.14.0',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+)
