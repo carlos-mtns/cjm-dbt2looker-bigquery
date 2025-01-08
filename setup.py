f02543c8f5dd29e6e4c373fb9979c3a395468e9c
carlos-mtns <carlos.martins@devoteam.com>
2025-01-08 10:53:39 +0100

    changed project version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cjm-dbt2looker-bigquery',
-    version='0.14.2',
+    version='0.15.0',
     packages=find_packages(),
     install_requires=[
 
